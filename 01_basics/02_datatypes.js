1ac257f5205a314e36b01f041c890ed1de3b9b46
Prashant singh <112719877+prashant923@users.noreply.github.com>
2024-02-04 18:54:06 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict";// treat all JS code as a newer version
+
+// alert(3+3)// we are using node js not browser
+//  in browser all JS engine runs in document so it will show pop
+// up like 6
+
+console.log(3+3)
+
+console.log("Prashant");
+
+let name ="Hitesh"
+let age = 18
+let isLoggedIn = true
+
+//primitive data types
+// int
+// string
+// boolean
+// null = standalone value = object
+
+// undefined
+
+
+console.log(typeof "prashant");
+console.log(typeof age);
+console.log(typeof null);// object
+console.log(typeof undefined);//undefined
