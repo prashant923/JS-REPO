aa0597c24b3ab4e4f521e1f6c0035092e70ee3c4
Prashant singh <112719877+prashant923@users.noreply.github.com>
2024-02-05 17:32:11 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score= "prashant"
+
+// console.log(typeof score)
+
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+
+// "33"-> 33
+// "33abc"-> NaN
+// undefined -> NaN
+// "true"-> 1
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
