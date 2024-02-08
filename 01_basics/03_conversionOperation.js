905f6c09d514881b3ba9e07fabf2675f5bbcb514
Prashant singh <112719877+prashant923@users.noreply.github.com>
2024-02-08 17:32:13 +0000

    prefix

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -15,4 +15,43 @@ let valueInNumber = Number(score)
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// ***********************Operations*************************
+ let value = 3
+ let negvalue = -value
+
+//  console.log(negvalue);
+//  console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "  Prashant"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+
+// console.log(+"");
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+
+let x= 3
+let y= x++;
+console.log(x,y);
